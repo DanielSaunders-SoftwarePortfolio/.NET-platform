6a13217e30bf76ab886f6c12413cb19eceae1a49
Daniel Saunders <131573288+DanielSaunders-SoftwarePortfolio@users.noreply.github.com>
2023-11-10 22:07:06 -0700

    MVC Movie Tutorial

---
+++ MvcMovie/MvcMovie/Migrations/20231111024505_ImageFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverPhotoPath",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverPhotoPath",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
